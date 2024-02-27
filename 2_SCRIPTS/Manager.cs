87da08250138100cfa45cd372b0ffff5674c76a0
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-02-27 14:33:10 +0000

    bug fixes, added UI

--- 2_SCRIPTS/Manager.cs
+++ 2_SCRIPTS/Manager.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//TMPro stands for TextMeshPro and handles text
-using TMPro;
-
 public class Manager : MonoBehaviour
 {
     public bool lost, spawning;
@@ -12,8 +9,9 @@ public class Manager : MonoBehaviour
     //A prefab is essentially a pre-made game object that you can repeatedly clone. Another good example of where you would use one is with bullets and projectiles
     public GameObject obstaclePrefab;
 
-    //This keeps track of all the projectiles currently on screen
+    //This keeps track of all the projectiles currently on screen, this is useful as to stop all obstacles from moving once the game ends
     public List<GameObject> activeObstacles;
+
     public float scrollSpeed, timeDelay, spawnX, minY, maxY;
     
 
@@ -22,14 +20,17 @@ public class Manager : MonoBehaviour
         activeObstacles.Remove(obstacle);
     }
 
+    //An IEnumerator is a couroutine (a block of code) that runs when you call it and has the ability to keep track of time, allowing for functions like WaitForSeconds(1)
     public IEnumerator spawnObstacle()
     {
+        //creates a new obstacle gameObject, then passes in values which weren't pre-set.
         GameObject newObstacle = Instantiate(obstaclePrefab);
         newObstacle.transform.position = new Vector3(spawnX, Random.Range(minY, maxY));
         newObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(-scrollSpeed, 0);
         newObstacle.GetComponent<Obstacles>().manager = this;
         activeObstacles.Add(newObstacle);
         yield return new WaitForSeconds(timeDelay);
+        //only repeats the coroutine if you havent lost yet.
         if (!lost)
         {
             spawning = false;
@@ -37,6 +38,7 @@ public class Manager : MonoBehaviour
         }
     }
 
+    //iterates through all obstacles on screen and makes them stop
     public void LoseGame()
     {
         lost = true;
--- 2_SCRIPTS/PlayerController.cs
+++ 2_SCRIPTS/PlayerController.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//SceneManagment is used to load different scenes as the name implies.  It is useful for loading new levels or resetting levels
 using UnityEngine.SceneManagement;
+
+//TMPro stands for TextMeshPro and handles text
+using TMPro;
+
 
 public class PlayerController : MonoBehaviour
 {
     public bool started, dead;
+    public int currentScore;
     public float jumpForce;
     public Vector2 spawnPos;
     public Manager manager;
+    public TextMeshProUGUI scoreText;
 
     private Rigidbody2D rb2d;
     
     // Start is called before the first frame update
     void Start()
     {
+        currentScore = 0;
         rb2d = GetComponent<Rigidbody2D>();
         manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
         started = false;
@@ -51,6 +60,16 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacles"))
         {
             dead = true;
+            manager.LoseGame();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Score"))
+        {
+            currentScore++;
+            scoreText.text = "Score: " + currentScore;
         }
     }
 }
